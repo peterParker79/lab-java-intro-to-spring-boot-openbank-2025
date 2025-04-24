d9a0a692b0647413940c45890a2aaeaaa1ceb400
peterParker79 <trypeterparker@gmail.com>
2025-04-24 15:11:08 +0200

    init structure proyect

---
+++ src/main/java/com/ironhack/lab_springboot_postman/LabSpringbootPostmanApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.lab_springboot_postman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpringbootPostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSpringbootPostmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/lab_springboot_postman/LabSpringbootPostmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.lab_springboot_postman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSpringbootPostmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
